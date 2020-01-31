13d0e85a1538916309f7712c1b79d6917a455929
Andy Hubbard <me@ansarya.com>
2020-01-30 21:54:58 -0500

    TileLayoutList

--- DQ3SFC/Program.cs
+++ DQ3SFC/Program.cs
@@ -1,17 +1,15 @@
 using logsmall;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace DQ3SFC {
 	class Program {
 
 		static void Main(string[] args) {
-			var rom = new Rom();
-
-			//-----------------------------------------------------
-			var startAddressPC = Rom.AddressToPC(0xc545ee);
+			var startAddress = 0xc545ee;
 			var length = 0xe;
-			var data = rom.Data.Span.Slice(startAddressPC, length);
+			var data = Rom.Slice(startAddress, length);
 
 			// $43d2,x	-	address
 			// $43b2,x	-	databank
@@ -19,10 +17,11 @@ namespace DQ3SFC {
 
 			// $4452,x	-	address
 			// $4432,x	-	databank
-			var xx3 = data.Long(3);
+			// points to a long that is the address of the TileLayoutList data
+			var tileListAddressPointer = data.Long(3);
 
 			// $44b2,x
-			var xx6 = data[6];
+			var xx6 = data.Byte(6);
 
 			// $44f2,x
 			var xx7 = data.Word(7);
@@ -34,48 +33,28 @@ namespace DQ3SFC {
 			var xxb = data.Word(0xb);
 
 			// $4612,x
-			var xxd = data[0xd] > 0x80 ? 0x8000 : data[0xd];
+			var xxd = data.Byte(0xd) > 0x80 ? 0x8000 : data.Byte(0xd);
 
 			//-----------------------------------------------------
 
-			var startAddress2PC = Rom.AddressToPC(xx3);
-			var graphicsAddressPC = Rom.AddressToPC(rom.Data.Span.Word(startAddress2PC));
+			/*
+C79D05 $A7 $4A         LDA [$4A] [F3541D] = $546E      A:00F3 X:0000 Y:000C S:06A9 D:199A DB:7E P:nvmxdIzc V:9   H:122
+C79D07 $9D $92 $44     STA $4492,X [7E4492] = $6DAB    A:546E X:0000 Y:000C S:06A9 D:199A DB:7E P:nvmxdIzc V:9   H:146
+C79D0A $A0 $02 $00     LDY #$0002                      A:546E X:0000 Y:000C S:06A9 D:199A DB:7E P:nvmxdIzc V:9   H:156
+C79D0D $B7 $4A         LDA [$4A],Y [F3541F] = $81F3    A:546E X:0000 Y:0002 S:06A9 D:199A DB:7E P:nvmxdIzc V:9   H:160
+C79D0F $29 $FF $00     AND #$00FF                      A:81F3 X:0000 Y:0002 S:06A9 D:199A DB:7E P:NvmxdIzc V:9   H:174
+C79D12 $9D $72 $44     STA $4472,X [7E4472] = $00EE    A:00F3 X:0000 Y:0002 S:06A9 D:199A DB:7E P:nvmxdIzc V:9   H:178
+			 */
+			// $4492,x	-	address
+			// $4472,x	-	databank
+			var tileListAddress = Rom.All.Long(tileListAddressPointer);
 
 			if (xx6 != 0x02) {
 				// does some other stuff at $c79d2d
 				// does some other stuff at $c79ea1 with graphicsAddress+2 (and the $dd89,X array)
 
-				// not actual number tiles copied, for example the slime is 4 tiles:
-				// endTileOffset is $12
-				// write one tile, --, write one tile --
-				// endTileOffset is $10
-				// address/command is $ff0e so skip $0e (destination address is increased by $0e tiles, endTileOffset is decreased by $0e)
-				// endTileOffset is $02
-				// write one tile, --, write one tile --
-				// endTileOffset is $00 so done
-				var endTileOffset = rom.Data.Span.Word(graphicsAddressPC + 2);
-
-				// around $c79d7a 
-				var tilesAddressListPC = graphicsAddressPC + 4;
-
-				// $c7c0b1
-				// address of the "bank" of graphics
-				var graphicsStartAddress = 0xce0080;
-
-				// $c7c0e6
-				//while (endTileOffset > 0) {
-				var tileNumber = rom.Data.Span.Word(tilesAddressListPC);
-				if (tileNumber >= 0xff00) {
-					var skip = tileNumber & 0xff00;
-					endTileOffset -= skip;
-				} else {
-					var tileAddress = graphicsStartAddress + (tileNumber << 5);
-
-
-
-					endTileOffset--;
-				}
-
+				// Uses TileBank.Primary
+				var tileList = new TileLayoutList(tileListAddress);
 
 
 
@@ -91,18 +70,61 @@ namespace DQ3SFC {
 		// tile numbers/offsets or skip destination commands
 		// $ffxx means skip xx tiles in destination, used in "total tile count" thing
 		class TileLayoutList {
-			
+			public int Address { get; set; }
+
+			// not actual number tiles copied, for example the slime is 4 tiles:
+			// endTileOffset is $12
+			// write one tile, --, write one tile --
+			// endTileOffset is $10
+			// address/command is $ff0e so skip $0e (destination address is increased by $0e tiles, endTileOffset is decreased by $0e)
+			// endTileOffset is $02
+			// write one tile, --, write one tile --
+			// endTileOffset is $00 so done
+			public int Size { get; set; }
+
+			public List<Item> Items { get; set; } = new List<Item>();
+
+			public TileLayoutList(int address) {
+				Address = address;
+				Refresh();
+			}
+
+			public void Refresh() {
+				var address = Address;
+				var size = Size = Rom.All.Word(address + 2);
+
+				address += 4;
+				while (size > 0) {
+					var value = Rom.All.Word(address);
+					var item = new Item {
+						IsTile = value < 0xff00,
+						Value = value
+					};
+
+					Items.Add(item);
+					address += 2;
+					size -= item.IsTile ? 1 : item.Skip;
+				}
+			}
+
+			public class Item {
+				public bool IsTile { get; set; }
+
+				public int Value { get; set; }
+
+				public int Skip { get => Value & 0x00ff; set => Value = value | 0xff00; }
+
+				public int Index { get => Value; }
+			}
 		}
 
-		interface ITileBank {
-
-		}
 
 		// Tiles are 8x8 4bpp planar composite 1-dimensional
 		class TileBank {
 			// Main graphics bank at $ce0080
 			// NumberOfTiles = 0xafc4
 			public static readonly TileBank Primary = new TileBank() {
+				// described in code at $c7c0b1
 				StartAddress = 0xce0080,
 				// This is a guess based on looking in TileMolester
 				// Non inclusive, last byte is at $e3f8ff
--- DQ3SFC/Statics.cs
+++ DQ3SFC/Statics.cs
@@ -10,8 +10,12 @@ namespace logsmall {
 
 		// offset is in bytes
 		// throws out of bound exceptions
-		public static int Word(this Span<byte> span, int offset) => (ushort)(span[offset] + (span[offset + 1] << 8));
+		public static int Byte(this Span<byte> span, int offset) => span[offset];
+		public static int Byte(this Memory<byte> memory, int offset) => memory.Span[offset];
+		public static int Word(this Span<byte> span, int offset) => (span[offset] + (span[offset + 1] << 8));
+		public static int Word(this Memory<byte> memory, int offset) => (memory.Span[offset] + (memory.Span[offset + 1] << 8));
 		public static int Long(this Span<byte> span, int offset) => span[offset] + (span[offset + 1] << 8) + (span[offset + 2] << 16);
+		public static int Long(this Memory<byte> memory, int offset) => memory.Span[offset] + (memory.Span[offset + 1] << 8) + (memory.Span[offset + 2] << 16);
 
 		//public static byte Bank(this int value) => (byte)((value & 0xff0000) >> 16);
 	}
